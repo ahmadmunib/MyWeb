e4b964fbba35e425e741812103fa6b0b8d51a74c
Ahmad Munib <94528926+ahmadmunib@users.noreply.github.com>
2024-04-19 21:21:46 +0400

    started to make the seperate page components

---
+++ resources/views/frontend/footers/sash/footer1.blade.php
@@ -0,0 +1,88 @@
+<section class="section landing-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4">
+                <div class="px-4">
+                    <h6 class="fw-semibold mb-3">ABOUT</h6>
+                    <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                        veritatis et quasi architecto beatae vitae dicta sunt
+                        explicabo.
+                    </p>
+                    <p class="mb-5 mb-lg-2 text-muted">Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat .
+                    </p>
+                </div>
+            </div>
+            <div class="col-xl-2">
+                <div class="px-4 mb-5 mb-lg-0">
+                    <h6 class="fw-semibold mb-3">PAGES</h6>
+                    <ul class="list-unstyled op-6 fw-medium landing-footer-list mb-0">
+                        <li>
+                            <a href="javascript:void(0);" class="">Email</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Profile</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Timeline</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Projects</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Contacts</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Portfolio</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-2">
+                <div class="px-4 mb-5 mb-lg-0">
+                    <h6 class="fw-semibold">INFO</h6>
+                    <ul class="list-unstyled op-6 fw-medium landing-footer-list mb-0">
+                        <li>
+                            <a href="javascript:void(0);" class="">Our Team</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Contact US</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">About</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Services</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Blog</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" class="">Terms &amp; Conditions</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="px-4">
+                    <a href="index.html"><img loading="lazy" alt="" class="logo-2 mb-3" src="{{ asset('themes/shash_b5/assets/images/brand-logos/desktop-logo.png') }}"></a>
+                    <a href="index.html"><img src="{{ asset('themes/shash_b5/assets/images/brand-logos/desktop-white.png') }}" class="logo-3 mb-3" alt="logo"></a>
+                    <p class="text-muted">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat.</p>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Enter your email" aria-label="Example text with button addon">
+                            <button class="btn btn-primary" type="button" id="button-addon2">Submit</button>
+                        </div>
+                    </div>
+                    <div class="btn-list mt-4">
+                        <button type="button" class="btn btn-icon rounded-pill"><i class="ri-facebook-line"></i></button>
+                        <button type="button" class="btn btn-icon rounded-pill"><i class="ri-youtube-line"></i></button>
+                        <button type="button" class="btn btn-icon rounded-pill"><i class="ri-twitter-line"></i></button>
+                        <button type="button" class="btn btn-icon rounded-pill"><i class="ri-instagram-line"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
